5b22467d2a0a806e914aad5f8460372e88008ee8
Amalia Hajarani <lalamalia2103@gmail.com>
2021-03-16 05:58:52 +0700

    15 menit ke-11; Menambahkan algoritma insertion sort
    Mulai membaca dan memahami soal ketiga. Masalah: kurang teliti dalam menelaah soal.

---
+++ Timing Searching and Sorting Algorithms/src/IntegerList.java
@@ -0,0 +1,138 @@
+// **************************************************************** 
+// IntegerList.java 
+// 
+// Define an IntegerList class with methods to create, fill, 
+// sort, and search in a list of integers. 
+// 
+// **************************************************************** 
+
+public class IntegerList 
+{ 
+    int[] list; //values in the list 
+    
+    //------------------------------------------------------- 
+    //create a list of the given size 
+    //------------------------------------------------------- 
+    public IntegerList(int size) 
+    { 
+        list = new int[size]; 
+    } 
+    
+    //------------------------------------------------------- 
+    //fill array with integers between 1 and 100, inclusive 
+    //------------------------------------------------------- 
+    public void randomize() 
+    { 
+        for (int i=0; i<list.length; i++) 
+            list[i] = (int)(Math.random() * 100) + 1; 
+    } 
+    
+    //------------------------------------------------------- 
+    //print array elements with indices 
+    //------------------------------------------------------- 
+    public void print() 
+    { 
+        for (int i=0; i<list.length; i++) 
+            System.out.println(i + ":\t" + list[i]); 
+    } 
+    
+    //------------------------------------------------------- 
+    //return the index of the first occurrence of target in the list. 
+    //return -1 if target does not appear in the list 
+    //------------------------------------------------------- 
+    public int search(int target) 
+    { 
+        int location = -1; 
+            for (int i=0; i<list.length && location == -1; i++) 
+                if (list[i] == target) 
+                    location = i; 
+        return location; 
+    } 
+    
+    //------------------------------------------------------- 
+    //sort the list into ascending order using the selection sort algorithm 
+    //------------------------------------------------------- 
+    public void selectionSortIncrease() 
+    { 
+        int minIndex; 
+        for (int i=0; i < list.length-1; i++) 
+        { 
+            //find smallest element in list starting at location i 
+            minIndex = i; 
+            for (int j = i+1; j < list.length; j++) 
+                if (list[j] < list[minIndex]) 
+                    minIndex = j; 
+            //swap list[i] with smallest element 
+            int temp = list[i]; 
+            list[i] = list[minIndex]; 
+            list[minIndex] = temp; 
+        } 
+    } 
+
+    public void selectionSortDecrease() 
+    { 
+        int maxIndex; 
+        for (int i=0; i < list.length-1; i++) 
+        { 
+            //find smallest element in list starting at location i 
+            maxIndex = i; 
+            for (int j = i+1; j < list.length; j++) 
+                if (list[j] > list[maxIndex]) 
+                    maxIndex = j; 
+            //swap list[i] with smallest element 
+            int temp = list[i]; 
+            list[i] = list[maxIndex]; 
+            list[maxIndex] = temp; 
+        } 
+    } 
+
+    public void insertionSort() 
+    {
+        for (int i = 1; i < list.length; i++) {
+            int key = list[i];
+            int j = i - 1;
+
+            while (j >= 0 && list[j] > key) {
+                list[j+1] = list[j];
+                j = j - 1;
+            }
+            list[j+1] = key;
+        }
+    }
+
+    public void replaceFirst(int oldVal, int newVal) {
+        int position = search(oldVal);
+
+        if(position == -1) {
+            throw new java.lang.Error("Value not found!");
+        } else {
+            list[position] = newVal;
+        }
+    }
+
+    public void replaceAll (int oldVal, int newVal) {
+        for (int i=0; i < list.length-1; i++) 
+            if (list[i] == oldVal)
+                list[i] = newVal;
+    }
+
+    public int binarySearchD (int target) {
+        int first = 0;
+        int last = list.length - 1;
+        int position = -1;
+
+        while (first <= last) {
+            int mid = (first + last) / 2;
+            if (list[mid] < target) {
+                first = mid + 1;
+            } else if (list[mid] > target) {
+                last= mid - 1;
+            } else if (list[mid] == target) {
+                position = mid;
+                break;
+            }
+        }
+        
+        return position;
+    }
+} 
---
+++ Timing Searching and Sorting Algorithms/src/IntegerListTest.java
@@ -0,0 +1,121 @@
+// **************************************************************** 
+// IntegerListTest.java 
+// 
+// Provide a menu-driven tester for the IntegerList class. 
+// 
+// **************************************************************** 
+import java.util.Scanner; 
+import java.util.*;
+
+public class IntegerListTest 
+{ 
+    static IntegerList list = new IntegerList(10); 
+    static Scanner scan = new Scanner(System.in); 
+    
+    //------------------------------------------------------- 
+    // Create a list, then repeatedly print the menu and do what the 
+    // user asks until they quit 
+    //------------------------------------------------------- 
+    public static void main(String[] args) 
+    { 
+        printMenu(); 
+        int choice = scan.nextInt(); 
+        while (choice != 0) 
+        { 
+            dispatch(choice); 
+            printMenu(); 
+            choice = scan.nextInt(); 
+        } 
+    } 
+    
+    //------------------------------------------------------- 
+    // Do what the menu item calls for 
+    //------------------------------------------------------- 
+    public static void dispatch(int choice) 
+    { 
+        int loc; 
+        int oldVal, newVal;
+
+        switch(choice) 
+        { 
+            case 0: 
+                System.out.println("Bye!"); 
+                break; 
+            case 1: 
+                System.out.println("How big should the list be?"); 
+                int size = scan.nextInt(); 
+                list = new IntegerList(size); 
+                list.randomize(); 
+                break; 
+            case 2: 
+                list.selectionSortIncrease(); 
+                break; 
+            case 3: 
+                list.selectionSortDecrease(); 
+                break; 
+            case 4: 
+                System.out.print("Enter the value to look for: "); 
+                loc = list.search(scan.nextInt()); 
+                if (loc != -1) 
+                    System.out.println("Found at location " + loc); 
+                else 
+                    System.out.println("Not in list"); 
+                break; 
+            case 5: 
+                list.print(); 
+                break; 
+            case 6:
+                Scanner scan1 = new Scanner(System.in); 
+                Scanner scan2 = new Scanner(System.in);
+
+                System.out.println("Insert value that you want to change: ");
+                oldVal = scan1.nextInt();
+                System.out.println("Insert value replacement: ");
+                newVal = scan1.nextInt();
+
+                list.replaceFirst(oldVal, newVal);
+                break;
+            case 7:
+                Scanner scan3 = new Scanner(System.in); 
+                Scanner scan4 = new Scanner(System.in);
+                int oldValue, newValue;
+
+                System.out.println("Insert value that you want to change: ");
+                oldVal = scan3.nextInt();
+                System.out.println("Insert value replacement: ");
+                newVal = scan4.nextInt();
+
+                list.replaceAll(oldVal, newVal);
+                break;
+            case 8:
+                System.out.print("Enter the value to look for: "); 
+                loc = list.binarySearchD(scan.nextInt()); 
+                if (loc != -1) 
+                    System.out.println("Found at location " + loc); 
+                else 
+                    System.out.println("Not in list"); 
+                break; 
+            default: 
+                System.out.println("Sorry, invalid choice"); 
+        } 
+    } 
+    
+    //------------------------------------------------------- 
+    // Print the user's choices 
+    //------------------------------------------------------- 
+    public static void printMenu() 
+    { 
+        System.out.println("\n Menu "); 
+        System.out.println(" ===="); 
+        System.out.println("0: Quit"); 
+        System.out.println("1: Create a new list (** do this first!! **)"); 
+        System.out.println("2: Sort the list using selection sort in increasing order"); 
+        System.out.println("3: Sort the list using selection sort in decreasing order"); 
+        System.out.println("4: Find an element in the list using linear search"); 
+        System.out.println("5: Print the list"); 
+        System.out.println("6: Change Value (only the first similar value will be changed)"); 
+        System.out.println("7: Change Value (all similar value will be changed)"); 
+        System.out.println("8: Find an element in the list using binary search"); 
+        System.out.print("\nEnter your choice: "); 
+    } 
+}
