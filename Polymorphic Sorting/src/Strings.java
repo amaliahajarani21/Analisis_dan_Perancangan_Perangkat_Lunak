626cacb4f51414315979a33c85e5addee35ee2f0
Amalia Hajarani <lalamalia2103@gmail.com>
2021-03-17 06:41:55 +0700

    15 menit ke-14
    Menyelesaikan perintah nomor 3 dan 4 soal ke empat

--- Polymorphic Sorting/src/Strings.java
+++ Polymorphic Sorting/src/Strings.java
@@ -5,7 +5,7 @@
 // ****************************************************** 
 import java.util.Scanner; 
 
-public class Numbers 
+public class Strings
 { 
     // -------------------------------------------- 
     // Reads in an array of integers, sorts them, 
@@ -13,24 +13,24 @@ public class Numbers
     // -------------------------------------------- 
     public static void main (String[] args) 
     { 
-        Integer[] intList; 
+        String[] stringList; 
         int size; 
 
         Scanner scan = new Scanner(System.in); 
         
-        System.out.print ("\nHow many integers do you want to sort? "); 
+        System.out.print ("\nHow many strings do you want to sort? "); 
         size = scan.nextInt(); 
-        intList = new Integer[size]; 
+        stringList = new String[size]; 
         
-        System.out.println ("\nEnter the numbers..."); 
+        System.out.println ("\nEnter the strings..."); 
         for (int i = 0; i < size; i++) 
-            intList[i] = scan.nextInt(); 
+            stringList[i] = scan.next();
         
-        // Sorting.selectionSort(intList); 
-        Sorting.insertionSort(intList); 
-        System.out.println ("\nYour numbers in sorted order..."); 
+        // Sorting.selectionSort(stringList); 
+        Sorting.insertionSort(stringList);
+        System.out.println ("\nYour strings in sorted order..."); 
         for (int i = 0; i < size; i++) 
-            System.out.print(intList[i] + " "); 
+            System.out.print(stringList[i] + " "); 
         System.out.println (); 
     } 
 }
