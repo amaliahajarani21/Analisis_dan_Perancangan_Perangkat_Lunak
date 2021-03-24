d1015cfc1ceb8af29ff371db508393372158e956
Amalia Hajarani <lalamalia2103@gmail.com>
2021-03-24 12:56:45 +0700

    15 menit ke-13: menyelesaikan soal ke-lima

--- Minggu 2/Copying a File/src/mainCopy.java
+++ Minggu 2/Copying a File/src/mainCopy.java
@@ -1,22 +1,31 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 public class mainCopy {
     public static void main(String[] args) {
         boolean found = false;
-        File file = new File("test1.txt");
 
-        try {
-            Scanner sc = new Scanner(file);
-
-            while (sc.hasNextLine()) {
-                int i = sc.nextInt();
-                System.out.println(i);
+        while (found == false){
+            try {
+                System.out.print("Enter the file name with extension : ");
+    
+                Scanner input = new Scanner(System.in);
+    
+                File file = new File(input.nextLine());
+    
+                input = new Scanner(file);
+    
+                while (input.hasNextLine()) {
+                    String line = input.nextLine();
+                    System.out.println(line);
+                }
+                input.close();
+                found = true;
+    
+            } catch (FileNotFoundException ex) {
+                System.out.println("File not found");
+                System.out.println("Insert Another file please");
             }
-            sc.close();
-        }catch (FileNotFoundException e) {
-            System.out.println("File not found");
         }
     }
 }
